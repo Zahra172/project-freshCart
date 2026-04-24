ce1cbd97fd8e3812c29ad97305c526fc42da759a
Zahra172 <ZaHra_magdi@protonmail.com>
2026-04-24 17:01:23 +0300

    refactor: streamline header management in WishlistContext for improved code clarity

--- src/Context/WishlistContext.jsx
+++ src/Context/WishlistContext.jsx
@@ -1,55 +1,70 @@
 import axios from "axios";
-import { createContext, useState ,useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const WishlistContext = createContext();
 export function WishlistContextProvider(props) {
-   const [wishlist, setWishlist] = useState([]);
-    let headers = {
-        token: localStorage.getItem("userToken"),
-      };
+  const [wishlist, setWishlist] = useState([]);
+  function getHeaders() {
+    return {
+      token: localStorage.getItem("userToken"),
+    };
+  }
 
-      function getWishlistItems() {
-        return axios.get(`https://ecommerce.routemisr.com/api/v1/wishlist`, {
-          headers: headers,
-        })
-          .then((response) => {
-            console.log(response);
-            setWishlist(response.data.data);
-            return response;
-          })
-          .catch((error) => {
-            throw error;
-          });
-      }
+  function getWishlistItems() {
+    return axios
+      .get(`https://ecommerce.routemisr.com/api/v1/wishlist`, {
+        headers: getHeaders(),
+      })
+      .then((response) => {
+        console.log(response);
+        setWishlist(response.data.data);
+        return response;
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }
 
-      function addToWishlist(productId) {
-        return axios.post(`https://ecommerce.routemisr.com/api/v1/wishlist`, { productId }, {
-            headers: headers,
-            })
-        .then((response) => {
-            console.log(response.data);
-            getWishlistItems();
-            return response;
-            })
-        .catch((error) => {
-            console.log(error);
-            throw error;
-        })};
-      function removeFromWishlist(productId){
-        return axios.delete(`https://ecommerce.routemisr.com/api/v1/wishlist/${productId}`,
-          { headers:headers }
-          ).then((response)=>{
-             getWishlistItems(); 
-            return response;
-          }).catch((error)=>{
-            throw error })
-      }
-      useEffect(() => {
-    getWishlistItems(); 
+  function addToWishlist(productId) {
+    return axios
+      .post(
+        `https://ecommerce.routemisr.com/api/v1/wishlist`,
+        { productId },
+        {
+          headers: getHeaders(),
+        },
+      )
+      .then((response) => {
+        console.log(response.data);
+        getWishlistItems();
+        return response;
+      })
+      .catch((error) => {
+        console.log(error);
+        throw error;
+      });
+  }
+  function removeFromWishlist(productId) {
+    return axios
+      .delete(`https://ecommerce.routemisr.com/api/v1/wishlist/${productId}`, {
+        headers: getHeaders(),
+      })
+      .then((response) => {
+        getWishlistItems();
+        return response;
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }
+  useEffect(() => {
+    getWishlistItems();
   }, []);
-return(
-    <WishlistContext.Provider value={{wishlist, getWishlistItems, addToWishlist, removeFromWishlist}}>
-        {props.children}
+  return (
+    <WishlistContext.Provider
+      value={{ wishlist, getWishlistItems, addToWishlist, removeFromWishlist }}
+    >
+      {props.children}
     </WishlistContext.Provider>
-)
+  );
 }
